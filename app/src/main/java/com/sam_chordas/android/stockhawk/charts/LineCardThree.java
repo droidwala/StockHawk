c5f44f3e27d1845c77d0c78b516d970866c73a69
messiahan <punitdama@gmail.com>
2016-03-21 21:53:08 +0530

    Sixty days line chart

---
+++ app/src/main/java/com/sam_chordas/android/stockhawk/charts/LineCardThree.java
@@ -0,0 +1,194 @@
+package com.sam_chordas.android.stockhawk.charts;
+
+import android.animation.PropertyValuesHolder;
+import android.content.Context;
+import android.graphics.Color;
+import android.os.Build;
+import android.support.v7.widget.CardView;
+import android.util.Log;
+import android.view.View;
+
+import com.db.chart.Tools;
+import com.db.chart.model.LineSet;
+import com.db.chart.view.AxisController;
+import com.db.chart.view.LineChartView;
+import com.db.chart.view.Tooltip;
+import com.db.chart.view.animation.Animation;
+import com.db.chart.view.animation.easing.BounceEase;
+import com.sam_chordas.android.stockhawk.R;
+
+import java.util.ArrayList;
+
+public class LineCardThree {
+
+
+    private final LineChartView mChart;
+
+    private final String[] labels;
+
+    private final float[] values;
+    private Context mContext;
+    private Tooltip mTip;
+    int min_value,max_value;
+    int MIN,MAX,STEP;
+    private static final String TAG = "LineCardThree";
+
+    public LineCardThree(CardView card,Context context,ArrayList<String> amounts,ArrayList<String> dates){
+        super();
+        mChart = (LineChartView) card.findViewById(R.id.sixty_days_line_chart);
+        mContext = context;
+        values = new float[amounts.size()];
+        labels = new String[amounts.size()];
+        for (int i = 0; i < dates.size(); i++) {
+            labels[i] = dates.get(i);
+        }
+        for (int j = 0; j < values.length; j++) {
+            values[j] = Float.parseFloat(amounts.get(j));
+        }
+        MinMaxAndStepLogic();
+
+    }
+
+    private void MinMaxAndStepLogic(){
+
+        min_value = (int) getMin_value(values);
+        max_value = (int) getMax_value(values);
+
+        //Min logic
+        if(min_value > 0 && min_value < 10){
+            MIN = 0;
+        }
+        else if(min_value > 9 && min_value < 99){
+            MIN = min_value - (min_value % 10);//2 digit
+        }
+        else if(min_value > 99 && min_value < 1000){
+            MIN = min_value - (min_value % 100);//3 digit
+        }
+        else if(min_value > 999 && min_value < 10000){
+            MIN = min_value - (min_value % 1000);//4 digit
+        }
+        else if(min_value > 9999 && max_value < 100000){
+            MIN = min_value - (min_value % 10000);// 5 digit
+        }
+        else if(min_value==0){
+            MIN = 0;
+        }
+
+        //Max logic
+        if(max_value > 0 && max_value < 9){
+            MAX = 10;
+        }
+        else if(max_value > 9 && max_value < 99){
+            MAX = (max_value - (max_value % 10)) + 10;//2 digit
+        }
+        else if(max_value > 99 && max_value < 1000){
+            MAX = (max_value - (max_value % 100)) + 100;//3 digit
+        }
+        else if(max_value > 999 && max_value < 10000){
+            MAX = (max_value - (max_value % 1000)) + 1000;//4 digit
+        }
+        else if(max_value > 9999 && max_value < 100000){
+            MAX = (max_value - (max_value % 10000)) + 10000;// 5 digit
+        }
+        else if(max_value == 0){
+            MAX = 0;
+        }
+
+        //STEP logic
+
+        STEP = GCD(MAX,MIN);
+
+        Log.d(TAG, "LineCardTwo: " + String.valueOf(MIN));
+    }
+
+    private int GCD(int a,int b){
+        if(b==0) return a;
+        return GCD(b,a%b);
+    }
+
+    private float getMin_value(float[] inputArray){
+
+        float minvalue = inputArray[0];
+
+        for(int i=1;i<=inputArray.length - 1;i++){
+            if(inputArray[i] < minvalue){
+                minvalue = inputArray[i];
+            }
+
+        }
+
+        return minvalue;
+    }
+
+    private float getMax_value(float[] inputArray){
+
+        float maxvalue = inputArray[0];
+
+        for (int i = 1; i <= inputArray.length - 1 ; i++) {
+            if(inputArray[i] > maxvalue){
+                maxvalue = inputArray[i];
+            }
+        }
+
+        //Log.d(TAG, "getMin_value: " + String.valueOf(inputArray.length));
+        return maxvalue;
+    }
+
+    public void show(){
+        LineSet dataSet = new LineSet(labels,values);
+        dataSet.setColor(Color.parseColor("#ad4b4b"))
+                .setFill(Color.parseColor("#ffcc6a"))
+                .setGradientFill(new int[]{Color.parseColor("#8c3838"), Color.parseColor("#a71717")}, null);
+
+        mChart.addData(dataSet);
+
+        mChart.setXLabels(AxisController.LabelPosition.OUTSIDE)
+                .setYLabels(AxisController.LabelPosition.OUTSIDE)
+                .setAxisLabelsSpacing(Tools.fromDpToPx(3))
+                .setLabelsColor(Color.parseColor("#FFFFFF"))
+                .setXAxis(true)
+                .setYAxis(true);
+        Log.d(TAG, "show: " + "MIN " + String.valueOf(MIN) +
+                " \nMAX " + String.valueOf(MAX) +
+                " \nSTEP " + String.valueOf(STEP));
+
+        if(MIN > 0 && MAX > 0) //For cases where stock price is less than 1.
+            mChart.setAxisBorderValues(MIN, MAX, STEP);
+
+        mTip = new Tooltip(mContext,R.layout.linechart_tooltip,R.id.value);
+
+        mTip.setVerticalAlignment(Tooltip.Alignment.BOTTOM_TOP);
+        mTip.setDimensions((int) Tools.fromDpToPx(80), (int) Tools.fromDpToPx(25));
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH) {
+
+            mTip.setEnterAnimation(PropertyValuesHolder.ofFloat(View.ALPHA, 1),
+                    PropertyValuesHolder.ofFloat(View.SCALE_Y, 1f),
+                    PropertyValuesHolder.ofFloat(View.SCALE_X, 1f)).setDuration(200);
+
+            mTip.setExitAnimation(PropertyValuesHolder.ofFloat(View.ALPHA, 0),
+                    PropertyValuesHolder.ofFloat(View.SCALE_Y, 0f),
+                    PropertyValuesHolder.ofFloat(View.SCALE_X, 0f)).setDuration(200);
+
+            mTip.setPivotX(Tools.fromDpToPx(65) / 2);
+            mTip.setPivotY(Tools.fromDpToPx(25));
+        }
+
+
+        mChart.setTooltips(mTip);
+
+        Runnable chartAction = new Runnable() {
+            @Override
+            public void run() {
+                mTip.prepare(mChart.getEntriesArea(0).get(3),values[3]);
+                mChart.showTooltip(mTip,true);
+            }
+        };
+
+
+        Animation anim = new Animation().setEasing(new BounceEase()).setEndAction(chartAction);
+
+        mChart.show(anim);
+    }
+}
+
--- app/src/main/java/com/sam_chordas/android/stockhawk/ui/StockDetailActivity.java
+++ app/src/main/java/com/sam_chordas/android/stockhawk/ui/StockDetailActivity.java
@@ -3,6 +3,7 @@ package com.sam_chordas.android.stockhawk.ui;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.CardView;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.widget.ArrayAdapter;
 
@@ -10,6 +11,7 @@ import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import com.sam_chordas.android.stockhawk.R;
 import com.sam_chordas.android.stockhawk.charts.LineCardOne;
+import com.sam_chordas.android.stockhawk.charts.LineCardThree;
 import com.sam_chordas.android.stockhawk.charts.LineCardTwo;
 import com.sam_chordas.android.stockhawk.charts.MPLineChart;
 import com.sam_chordas.android.stockhawk.pojo.History;
@@ -35,16 +37,29 @@ public class StockDetailActivity extends AppCompatActivity{
     private String BASE_URL = "https://query.yahooapis.com/v1/public/yql?q=";
     private String startDate = "",endDate = "",today_date="",past_thirty="",past_sixty="";
     private String stock_name;
+    private Toolbar toolbar;
+
+     ArrayList<String> monthly_close_amt = new ArrayList<String>();
+     ArrayList<String> monthly_dates = new ArrayList<String>();
+
+    ArrayList<String> sixty_close_amt = new ArrayList<String>();
+    ArrayList<String> sixty_dates = new ArrayList<String>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.stock_detail);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
         Bundle b = getIntent().getExtras();
         stock_name = b.getString("STOCK");
         Log.d(TAG, "onCreate: called " + b.getString("STOCK"));
 
-        InitializeStartAndEndDate();//Get start and end dates.
+
+        InitializeStartAndEndDate();//Get start and end dates for weekly data
+        Thiry_And_Sixty();//for 30 days and 60 days
 
         //Create weekly api query for the given stock name
         StringBuilder urlString = new StringBuilder();
@@ -100,7 +115,7 @@ public class StockDetailActivity extends AppCompatActivity{
                     @Override
                     public void run() {
                         //(new LineCardOne((CardView)findViewById(R.id.weekly_card),StockDetailActivity.this,close_amt)).show();
-                        PastThirty();
+
                         StringBuilder urlString = new StringBuilder();
                         urlString.append(BASE_URL);
                         try {
@@ -142,48 +157,118 @@ public class StockDetailActivity extends AppCompatActivity{
             public void onResponse(Response response) throws IOException {
                 //Log.d(TAG, "onResponse: called " + response.body().string());
                 Gson gson = new Gson();
-                History history = gson.fromJson(response.body().string(),new TypeToken<History>(){}.getType());
-                if(history.getQuery().getResults()!= null) {
+                History history = gson.fromJson(response.body().string(), new TypeToken<History>() {
+                }.getType());
+                if (history.getQuery().getResults() != null) {
                     ArrayList<History.QueryEntity.ResultsEntity.QuoteEntity> quotes = (ArrayList<History.QueryEntity.ResultsEntity.QuoteEntity>) history.getQuery().getResults().getQuote();
 
                     Log.d(TAG, "onResponse: after parsing " + String.valueOf(quotes.size()));
 
-                    final ArrayList<String> monthly_close_amt = new ArrayList<String>();
-                    final ArrayList<String> dates = new ArrayList<String>();
+
                     for (int i = 0; i < quotes.size(); i++) {
-                        Log.d(TAG, "Data: " +  quotes.get(i).getDate() + " " + quotes.get(i).getAdj_Close());
-                        if(i==0){
-                            dates.add(quotes.get(i).getDate());
-                        }
-                        else if(i == quotes.size()-1){
-                            dates.add(quotes.get(i).getDate());
-                        }
-                        else{
-                            dates.add("");
+                        Log.d(TAG, "Data: " + quotes.get(i).getDate() + " " + quotes.get(i).getAdj_Close());
+                        if (i == 0) {
+                            monthly_dates.add(quotes.get(i).getDate());
+                        } else if (i == quotes.size() - 1) {
+                            monthly_dates.add(quotes.get(i).getDate());
+                        } else {
+                            monthly_dates.add("");
                         }
                         monthly_close_amt.add(quotes.get(i).getAdj_Close());
                     }
 
                     Collections.reverse(monthly_close_amt);
-                    Collections.reverse(dates);
+                    Collections.reverse(monthly_dates);
                     runOnUiThread(new Runnable() {
                         @Override
                         public void run() {
-                            new LineCardTwo((CardView) findViewById(R.id.monthly_card), StockDetailActivity.this, monthly_close_amt,dates).show();
+                          //  new LineCardTwo((CardView) findViewById(R.id.monthly_card), StockDetailActivity.this, monthly_close_amt, monthly_dates).show();
                             // new MPLineChart((CardView)findViewById(R.id.monthly_card),StockDetailActivity.this,monthly_close_amt).show();
+                            StringBuilder urlString = new StringBuilder();
+                            urlString.append(BASE_URL);
+                            try {
+                                urlString.append(URLEncoder.encode("select * from yahoo.finance.historicaldata where symbol = ","UTF-8"));
+                                urlString.append(URLEncoder.encode("\"" + stock_name + "\"","UTF-8"));
+                                urlString.append(URLEncoder.encode(" and startDate = " + "\"" + past_sixty + "\"","UTF-8"));
+                                urlString.append(URLEncoder.encode(" and endDate = " + "\"" + today_date + "\"","UTF-8"));
+                                urlString.append("&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=");
+
+                            } catch (UnsupportedEncodingException e) {
+                                e.printStackTrace();
+                            }
+
+                            try {
+                                FetchSixtyData(urlString.toString());
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
                         }
                     });
+                } else {
+                    Log.d(TAG, "onResponse: No data!!");
                 }
-                else{
-                    Log.d(TAG, "onResponse: No data!!" );
-                }
-
 
 
             }
         });
     }
 
+
+
+    private void FetchSixtyData(String url) throws IOException{
+
+        Request request = new Request.Builder()
+                .url(url).build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Request request, IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Response response) throws IOException {
+                //Log.d(TAG, "onResponse: called " + response.body().string());
+                Gson gson = new Gson();
+                History history = gson.fromJson(response.body().string(), new TypeToken<History>() {
+                }.getType());
+                if (history.getQuery().getResults() != null) {
+                    ArrayList<History.QueryEntity.ResultsEntity.QuoteEntity> quotes = (ArrayList<History.QueryEntity.ResultsEntity.QuoteEntity>) history.getQuery().getResults().getQuote();
+
+                    Log.d(TAG, "onResponse: after parsing " + String.valueOf(quotes.size()));
+
+
+                    for (int i = 0; i < quotes.size(); i++) {
+                        Log.d(TAG, "Data: " + quotes.get(i).getDate() + " " + quotes.get(i).getAdj_Close());
+                        if (i == 0) {
+                            sixty_dates.add(quotes.get(i).getDate());
+                        } else if (i == quotes.size() - 1) {
+                            sixty_dates.add(quotes.get(i).getDate());
+                        } else {
+                            sixty_dates.add("");
+                        }
+                        sixty_close_amt.add(quotes.get(i).getAdj_Close());
+                    }
+
+                    Collections.reverse(sixty_close_amt);
+                    Collections.reverse(sixty_dates);
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            new LineCardTwo((CardView) findViewById(R.id.monthly_card), StockDetailActivity.this, monthly_close_amt, monthly_dates).show();
+                            new LineCardThree((CardView) findViewById(R.id.sixty_days_card), StockDetailActivity.this, sixty_close_amt, sixty_dates).show();
+
+                            // new MPLineChart((CardView)findViewById(R.id.monthly_card),StockDetailActivity.this,monthly_close_amt).show();
+                        }
+                    });
+                } else {
+                    Log.d(TAG, "onResponse: No data!!");
+                }
+
+
+            }
+        });
+    }
     private void InitializeStartAndEndDate(){
         Calendar c = Calendar.getInstance();
         c.set(Calendar.DAY_OF_WEEK, Calendar.MONDAY);
@@ -191,16 +276,22 @@ public class StockDetailActivity extends AppCompatActivity{
         startDate = df.format(c.getTime());
         c.add(Calendar.DATE,4);
         endDate = df.format(c.getTime());
-        Log.d(TAG, "Start Date : " + startDate + "    " + "End Date : " + endDate );
+        Log.d(TAG, "Start Date : " + startDate + "    " + "End Date : " + endDate);
     }
 
-    private void PastThirty(){
+    private void Thiry_And_Sixty(){
         Calendar c = Calendar.getInstance();
         DateFormat df = new SimpleDateFormat("yyyy-MM-dd",Locale.getDefault());
         today_date = df.format(c.getTime());
         c.add(Calendar.DATE,-30);
         past_thirty = df.format(c.getTime());
-        Log.d(TAG, "PastThirty: Today's date " + today_date + " thirty days ago " + past_thirty);
+        c.add(Calendar.DATE,-30);
+        past_sixty = df.format(c.getTime());
+        Log.d(TAG, "PastThirty: Today's date " + today_date + " thirty days ago " + past_thirty +
+        " sixty days ago " + past_sixty);
     }
+
+
+
 }
 
